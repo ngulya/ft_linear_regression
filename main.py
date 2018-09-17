960f3ff50317d72caa13b47515963f6937a4f51e
Nazariy Gulya <ngulya@e1r10p3.unit.ua>
2018-09-17 15:59:40 +0300

    graph

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from time import time
 import sys
 import numpy as np
 from dop_function import MSE, RMSE, load_theta, save_theta ,return_list
-import random
+import random as rand
+import Tkinter as T
 
 def estimatePrice(mileage):
 	global th0, th1
@@ -31,18 +32,40 @@ def predict(l_km):
 	for i in l_km:
 		lst.append(estimatePrice(i))
 	return lst
+# https://habr.com/post/163395/
+def graph():
+	root = T.Tk()
+	canv = T.Canvas(root, width = 1000, height = 1000, bg = "lightblue")#, cursor = "pencil")
+	# canv.create_line(500,1000,500,0,width=2,arrow='last') 
+	canv.create_line(0,500,1000,500,width=2,arrow='last')
+	# x0=0,y0=500,x1=1000,y1=500
+	# x = 10
+	# y = 10
+	# i = len(l_km) - 1
+	# maxx = max(l_km)
+	# minn = min(l_km)
+	# canv.create_oval(x, y, x + 20, y + 20, fill = 'black')
+
+	canv.pack()
+	root.mainloop() 
 
 if __name__ == "__main__":
 	l_km, l_price = return_list()
+	
+	graph()
+	exit()
 	print MSE(l_km, l_price)
 	print RMSE(l_km, l_price)
 	# save_theta(0.1124142111, -0.3)
 	th0, th1, status = load_theta()
 	if status == False:
-		th0 = random.uniform(-0.49, 0.49)
-		th1 = random.uniform(-0.49, 0.49)
+		th0 = rand.uniform(-0.49, 0.49)
+		th1 = rand.uniform(-0.49, 0.49)
 	print 'MSE = ', MSE(predict(l_km), l_price)
 	th0, th1 = trainModel(l_km,l_price, 0.9)
 	print 'MSE = ', MSE(predict(l_km), l_price)
+	# ans = raw_input('Save theta Y-Yes')
+	# if ans == 'Y' or ans == 'y':
+	# 	save_theta(th0, th1)
 	
-	# print th0, th1
+
