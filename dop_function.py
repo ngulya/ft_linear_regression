e9b71fb3add7b474016cd376f2cd1aee49a9d9b1
ngulya <nazik_bazik@icloud.com>
2018-09-19 22:22:39 +0300

    +graph

--- dop_function.py
+++ dop_function.py
@@ -1,8 +1,97 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+########PYTHON2
 
 from time import time
 import sys
 import numpy as np
+import tkinter as T
+# import Tkinter as T 2
+def ERR(st):
+	print(st)
+	sys.exit(1)
+
+def predict(l_km, th0, th1):
+	lst = []
+	for i in l_km:
+		lst.append(estimatePrice(i, th0, th1))
+	return lst
+def estimatePrice(mileage,th0, th1):
+	return th0 + (th1 * mileage)
+
+def trainModel(l_km, l_price, learningRate,th0, th1):
+	lens = len(l_km)
+	sums0 = 0
+	sums1 = 0
+	i = 0
+	while i < lens:
+		tmp = estimatePrice(l_km[i], th0, th1) - l_price[i]
+		sums0 += tmp
+		sums1 += (tmp * l_km[i])
+		i += 1
+
+	th0 = learningRate * (sums0/i)
+	th1 = learningRate * (sums1/i)
+
+	return th0, th1
+
+
+def graph(l_km, l_price, th0, th1):
+	root = T.Tk()
+	canv = T.Canvas(root, width = 1000, height = 1000, bg = "lightblue")
+	canv.create_line(3,1000,3,0,width=2,arrow='last') 
+	canv.create_line(0,997,1000,997,width=2,arrow='last')
+	maxx_km = max(l_km)
+	minn_km = min(l_km)
+	maxx_price = max(l_price)
+	minn_price = min(l_price)
+	
+	rz = maxx_km - minn_km
+	rz /= 20
+	i = 10
+	start = minn_km
+	while start < maxx_km-rz-1:
+		canv.create_text(30,i,text = int(start))
+		canv.create_line(0,i,6,i,width=2, fill = 'black')
+		start += rz
+		i += 50
+
+	rz = maxx_price - minn_price
+	rz /= 10
+	i = 100
+	start = minn_price + rz
+	while start < maxx_price:
+		canv.create_text(i,980,text = int(start))
+		canv.create_line(i,994,i,1000,width=2, fill = 'black')
+		start += rz
+		i += 100
+
+	lens = len(l_price)
+	i = 0
+	while i < lens:
+		y = (float(l_km[i] - minn_km) / (maxx_km - minn_km))*1000
+		x = (float(l_price[i] - minn_price) / (maxx_price - minn_price))*1000
+		canv.create_oval(x, y, x + 10, y + 10, fill = 'black')
+		i += 1
+	
+	pr = predict(l_km, th0, th1)
+	maxx_price = max(pr)
+	minn_price = min(pr)
+	lens = len(l_price)
+	i = 0
+	lsty = []
+	lstx = []
+	while i < lens:
+		y = (float(l_km[i] - minn_km) / (maxx_km - minn_km))*1000
+		x = (float(pr[i] - minn_price) / (maxx_price - minn_price))*1000
+		lsty.append(y)
+		lstx.append(x)
+		i += 1
+	minx = lstx.index(min(lstx))
+	maxx = lstx.index(max(lstx))
+
+	canv.create_line(lstx[minx],lsty[minx],lstx[maxx],lsty[maxx],width=5, fill = 'orange')
+	canv.pack()
+	root.mainloop() 
 
 def MSE(have, must):
 	lens = len(have) - 1
@@ -29,7 +118,7 @@ def load_theta():
 		try:
 			float(line)
 		except Exception as e:
-			print 'No theta'
+			print('No theta')
 			return 0,0,False
 		else:
 			if zr == 0:
@@ -37,7 +126,7 @@ def load_theta():
 			elif zr == 1:
 				th1 = float(line)
 			else:
-				print 'No theta'
+				print('No theta')
 				return 0,0,False
 			zr += 1
 	return th0, th1, True
@@ -48,38 +137,35 @@ def save_theta(th0, th1):
 		in_file = open('theta', "w")
 	except:
 		ERR('Error: cant read file: theta')
-		sys.exit(1)
 
 	in_file.write(str(th0) + '\n')
 	in_file.write(str(th1))
-
-
-def return_list():
-
-	l_km, l_price = [],[]
-	try:
-		in_file = open('data.csv', "r")
-	except:
-		ERR('Error: cant read file: data.csv')
-		sys.exit(1)
-
-	for line in in_file.readlines():
-		# print line
-		try:
-			line2 = line.split(',')
-			int(line2[0])
-			int(line2[1])
-		except Exception as e:
-			if line.find('km,price') != 0:
-				print 'line != km,price'
-				sys.exit(1)
-		else:
-			l_km.append(int(line2[0]))
-			l_price.append(int(line2[1]))
-
 	in_file.close()
-	if len(l_km) == 0 or len(l_price) == 0:
-		print '0 size'
-		sys.exit(1)
 
-	return l_km, l_price
+
+def return_list_from_data(name_csv):
+
+	l_x, l_y = [],[]
+	try:
+		in_file = open(name_csv, "r")
+	except:
+		ERR('Error: cant read file:' + name_csv)
+	num_str = 0
+	frst = 0
+	for line in in_file.readlines():
+		try:
+			line = line.split(',')
+			int(line[0])
+			int(line[1])
+		except Exception as e:
+			if frst != 0:
+				ERR('Error: num str = ' + str(frst + 1))
+			num_str += 1
+		else:
+			l_x.append(int(line[0]))
+			l_y.append(int(line[1]))
+		frst += 1
+	in_file.close()
+	if len(l_x) == 0 or len(l_y) == 0 or len(l_y) != len(l_x):
+		ERR('Error: zero size')
+	return l_x, l_y
--- main.py
+++ main.py
@@ -1,71 +1,35 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from time import time
 import sys
 import numpy as np
-from dop_function import MSE, RMSE, load_theta, save_theta ,return_list
-import random as rand
-import Tkinter as T
+from dop_function import MSE, RMSE, load_theta, save_theta,estimatePrice, trainModel,return_list_from_data,graph,predict
+import tkinter as T
+# import Tkinter as T 2
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
 
-def estimatePrice(mileage):
-	global th0, th1
-	res = th0 + (th1 * mileage)
-	return res
-
-def trainModel(l_km, l_price, learningRate):
-	i = len(l_km) - 1
-	sums0 = 0
-	sums1 = 0
-	while i >= 0:
-		tmp = estimatePrice(l_km[i]) - l_price[i]
-		sums0 += tmp
-		sums1 += (tmp * l_km[i])
-		i -= 1
-	i = len(l_km) - 1
-	th0 = learningRate * (sums0/i)
-	th1 = learningRate * (sums1/i)
-
-	return th0, th1
-
-def predict(l_km):
-	lst = []
-	for i in l_km:
-		lst.append(estimatePrice(i))
-	return lst
-# https://habr.com/post/163395/
-def graph():
-	root = T.Tk()
-	canv = T.Canvas(root, width = 1000, height = 1000, bg = "lightblue")#, cursor = "pencil")
-	# canv.create_line(500,1000,500,0,width=2,arrow='last') 
-	canv.create_line(0,500,1000,500,width=2,arrow='last')
-	# x0=0,y0=500,x1=1000,y1=500
-	# x = 10
-	# y = 10
-	# i = len(l_km) - 1
-	# maxx = max(l_km)
-	# minn = min(l_km)
-	# canv.create_oval(x, y, x + 20, y + 20, fill = 'black')
-
-	canv.pack()
-	root.mainloop() 
 
 if __name__ == "__main__":
-	l_km, l_price = return_list()
-	
-	graph()
-	exit()
-	print MSE(l_km, l_price)
-	print RMSE(l_km, l_price)
-	# save_theta(0.1124142111, -0.3)
+	# l_km, l_price = return_list_km_price()
+	l_km, l_price = return_list_from_data('data.csv')
+	# plt.plot(l_km, l_price, 'ro')
+	# plt.show()
+	# # exit()
+
 	th0, th1, status = load_theta()
 	if status == False:
-		th0 = rand.uniform(-0.49, 0.49)
-		th1 = rand.uniform(-0.49, 0.49)
-	print 'MSE = ', MSE(predict(l_km), l_price)
-	th0, th1 = trainModel(l_km,l_price, 0.9)
-	print 'MSE = ', MSE(predict(l_km), l_price)
+		th0 = 0
+		th1 = 0
+	
+	# graph(l_km, l_price, th0, th1)
+	print ('MSE = ', MSE(predict(l_km, th0, th1), l_price))
+	th0, th1 = trainModel(l_km,l_price, 0.000000003,th0, th1)
+	print ('MSE = ', MSE(predict(l_km, th0, th1), l_price))
+	
+	graph(l_km, l_price, th0, th1)
 	# ans = raw_input('Save theta Y-Yes')
 	# if ans == 'Y' or ans == 'y':
-	# 	save_theta(th0, th1)
+	# save_theta(th0, th1)
 	
 
